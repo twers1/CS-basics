7d379726423f48837d7817c531abc7a8cd5bb6a7
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2023-01-08 19:07:21 +0300

    Create variables.cs

---
+++ day1/variables.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light
+{
+    internal class Program
+    {
+  
+
+        static void Main(string[] args)
+        {
+            // Целочисленные типы 
+            byte b; // от 0 до 255 
+            sbyte sb; // -128 от 127
+            short s; // -32768 до 32767
+            ushort us; // от 0 до 65535
+            int i; // от -2147483648 до 214748347
+            uint ui; // от 0 до 4294672295
+            long l = long.MaxValue; // смотрит какое максимальное значение имеем лонг, но повседневно не используют тип long
+            ulong ul;
+            // Числа с плавающей точкой 
+            float f;
+            double d;
+            // Символьный тип 
+            char c = ' ';
+            // Строковый тип 
+            string str = "что, смотришь в мой репозиторий?:)";
+            // Булевый(логический) тип 
+            bool bl;
+
+            // Примеры 
+            int age = 17;
+
+
+            int age2;
+            age2 = 17;
+
+            // Основые типы - int, float, char, string
+        }
+    }
+}
