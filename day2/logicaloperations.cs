2c1f00fba60ecb5d179609cfac78ee8f83d475f4
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2023-01-09 15:11:14 +0300

    Create logicaloperations.cs

---
+++ day2/logicaloperations.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light
+{
+    internal class Program
+    {
+  
+
+        static void Main(string[] args)
+        {
+            // Логические операторы: ==, !=, >, >=, <=, <
+            int age = 18;
+            bool accesIsAllowed;
+
+            accesIsAllowed = age >= 18;
+            Console.WriteLine(accesIsAllowed);
+        }
+    }
+}
