8ec2b077b87dc68695db01b81d10f63a28618221
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2023-01-10 20:54:27 +0300

    Create concantation.cs

---
+++ day3/concantation.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light
+{
+    internal class Program
+    {
+  
+
+        static void Main(string[] args)
+        {
+            // Контантенация - это сложение двух или более слов
+            string greeting = "Привет";
+            string name = "Сима";
+            string message = greeting + " " +  name;
+
+            Console.WriteLine(message, ", рад знакомству");
+        }
+    }
+}
