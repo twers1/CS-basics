a45dc86e7895c8b9f55b7e0b794c7e67597fc0fe
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2023-01-27 19:01:51 +0300

    updating tasks

--- day12/collegetasks.cs
+++ day12/collegetasks.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,23 +17,26 @@ namespace Light
 
 
             int[] array = new int[10];
+            int sum = 0;
             Console.WriteLine("Введи мне произвольный размер массива(10 чисел)");
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array[i] = Convert.ToInt32(Console.ReadLine()); // пользователь вводит числа, которые будут находится в массиве
             }
             int min = array[0]; // переменная min присваивается значение, которое равен нулю
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < array.Length; i++)
 
             {
                 if (array[i] < min)
                 {
                     min = array[i];
 
+
                 }
+                sum += array[i];
             }
             int max = array[9];
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < array.Length; i++)
             {
 
                 if (array[i] > max)
@@ -43,28 +47,32 @@ namespace Light
             }
             Console.WriteLine("Минимальное число: {0}", min);
             Console.WriteLine("Максимальное число: {0}", max);
-            Console.WriteLine("Сумма максимального и минимального числа: " + (min + max));
+            Console.WriteLine("Сумма всех элементов: " + sum);
 
             // Задание 2 (2)
-            Random rand = new Random();
 
-            int[,] array2 = new int[5, 5];
+            Console.WriteLine("Введи размер таблицы: ");
 
-            Console.WriteLine("Введи мне произвольный размер двумерного массива 5 на 5");
-            for (int i = 0; i < 5; i++)
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] array2 = new int[m, n];
+
+            Console.WriteLine("Введите мне элементы: ");
+            for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    array2[i, j] = rand.Next(0, 10);
+                    array2[i, j] = int.Parse(Console.ReadLine());
                 }
             }
             Console.WriteLine("Вывод таблицы: ");
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < m; i++)
             {
                 Console.WriteLine();
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    Console.WriteLine("{0}\t", array2[i, j]);
+                    Console.Write("{0}\t", array2[i, j]);
                 }
             }
 
@@ -72,14 +80,16 @@ namespace Light
             // Задание 2 (3)
 
             int[] array3 = new int[10];
+            // int sum = 0; - уже есть данная переменная в первом задании
             Console.WriteLine("Введи мне произвольный размер массива(10 чисел)");
 
             for (int i = 0; i < 10; i++)
             {
                 array3[i] = Convert.ToInt32(Console.ReadLine()); // пользователь вводит числа, которые будут находится в массиве
+                sum+= array3[i];
             }
 
-            int print = (array3[0] + array3[1] + array3[2] + array3[3] + array3[4] + array3[5] + array3[6] + array3[7] + array3[8] + array3[9]) / array3.Length;
+            int print = sum / array3.Length;
             Console.WriteLine("Среднее арифметическое значение чисел в массиве: " + print);
         }
     }
